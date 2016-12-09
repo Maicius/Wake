8dd712e67aa4f38d2e2ed326f4656f268506348a
DESKTOP-1VJF0IT\Maicius <maicius@outlook.com>
2016-12-10 02:28:21 +0800

    完善register的防错设置，密码长度为6-16位，用户名全为数字和大小写字母的组合

--- CRAZYCLOCK/app/src/main/java/com/maicius/wake/InterChange/Register.java
+++ CRAZYCLOCK/app/src/main/java/com/maicius/wake/InterChange/Register.java
@@ -4,15 +4,17 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.TextView;
 import com.maicius.wake.alarmClock.R;
 import com.maicius.wake.web.WebService;
+import java.util.regex.Pattern;
 
 public class Register extends Activity {
     private String info;
@@ -24,20 +26,47 @@ public class Register extends Activity {
         super.onCreate(savedInstanceState);
         Log.v("maicius", "enter sign in");
         setContentView(R.layout.register);
+        TextView Login = (TextView) findViewById(R.id.login);
         Button register_button = (Button) findViewById(R.id.sign_up_button);
         username = (EditText) findViewById(R.id.register_username);
         password = (EditText) findViewById(R.id.register_password);
+        password_confirm = (EditText) findViewById(R.id.register_password_confirm);
+        Login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LogIn();
+            }
+        });
 
         register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog = new ProgressDialog(Register.this);
-                dialog.setTitle("提示");
-                dialog.setMessage("正在注册，请稍后...");
-                dialog.setCancelable(false);
-                dialog.show();
-                //创建子线程
-                new Thread(new SignUpThread()).start();
+                if (username.getText().toString().equals("")){
+                    raiseAlertDialog("提示","用户名不能为空");
+                }
+                else if(!isUserName(username.getText().toString())){
+                    raiseAlertDialog("提示","");
+
+                }
+                else if (password_confirm.getText().toString().equals("")
+                        || password.getText().toString().equals("")) {
+                    raiseAlertDialog("提示","密码不能为空");
+                }
+                else if(password.getText().toString().length()<6
+                        || password.getText().toString().length()>16){
+                    raiseAlertDialog("提示","密码长度必须在6-16之间");
+                }
+                else if (!(password_confirm.getText().toString().equals(password.getText().toString()))) {
+                    raiseAlertDialog("提示","两次密码不一致");
+                }
+                else{
+                    dialog = new ProgressDialog(Register.this);
+                    dialog.setTitle("提示");
+                    dialog.setMessage("正在注册，请稍后...");
+                    dialog.setCancelable(false);
+                    dialog.show();
+                    //创建子线程
+                    new Thread(new SignUpThread()).start();
+                }
             }
         });
     }
@@ -52,22 +81,40 @@ public class Register extends Activity {
                     dialog.dismiss();
 
                     if (info.equals("success")) {
-                        Register.this.finish();
-                        setContentView(R.layout.log_in);
-                    }
-                    else if (info.equals("failed")) {
                         AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register.this);
-                        alertDialog.setTitle("注册信息").setMessage("注册失败！");
+                        alertDialog.setTitle("注册成功").setMessage("欢迎来到Wake！");
                         alertDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-
+                                Register.this.finish();
+                                setContentView(R.layout.log_in);
                             }
                         });
                         alertDialog.create().show();
+
+                    }
+                    else if (info.equals("failed")) {
+                        raiseAlertDialog("注册信息","Sorry, 注册失败");
                     }
                 }
             });
         }
+    }
+    private void LogIn() {
+        startActivity(new Intent(this, LogIn.class));
+    }
+    private boolean isUserName(String username){
+        return Pattern.matches("^[a-zA-Z]\\w{5,17}$", username);
+    }
+
+    private void raiseAlertDialog(String title, String message){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register.this);
+        alertDialog.setTitle(title).setMessage(message);
+        alertDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alertDialog.create().show();
     }
 }
